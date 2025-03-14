4598e63cb35fa54f9e1d93aa1e792a8d743d25e7
Fawwaz Thoerif <fawwazthoerif@gmail.com>
2025-03-14 09:11:30 +0700

    ✨ (config.py): add configuration file for captcha settings with support for multiple 3rd party providers and apikey configuration.

---
+++ config.py
@@ -0,0 +1,15 @@
+# CAPTCHA CONFIG
+
+# You must fill in the apikey and the name of the 3rd party provider.
+
+# 3rd party supported by this program
+
+# 1. anticaptcha
+# 2. twocaptcha
+# 3. capsolver
+
+# and don't forget to fill in apikey
+
+#
+APIKEY = ""
+PROVIDER = "twocaptcha"
